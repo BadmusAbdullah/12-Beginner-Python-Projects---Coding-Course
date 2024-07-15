7a5626001bd4e74a457337a52b04c55231d3a004
Badmus Abdullah <badmusakanbi@gmail.com>
2024-07-15 21:52:11 +0100

    first commit

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adj = input('Enter an adjective: ')
+verb1 = input('Enter the first verb: ')
+verb2 = input('Enter the second verb: ')
+famous_person = input('Enter the name of a famous person: ')
+
+madlib = f'Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydraded and {verb2} like you are {famous_person}!'
+print(madlib)
